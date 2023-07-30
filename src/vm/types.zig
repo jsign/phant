32e069428286b1a0b5758c4b185dd2f75381807f
Ignacio Hagopian <jsign.uy@gmail.com>
2023-07-30 13:43:23 -0300

    vm/types: fix address type and add Transaction type

--- src/vm/types.zig
+++ src/vm/types.zig
@@ -7,7 +7,45 @@ const fmtSliceHexLower = std.fmt.fmtSliceHexLower;
 const util = @import("../util/util.zig");
 
 pub const Bytecode = []const u8;
-pub const Address = [32]u8;
+pub const Address = [20]u8;
+
+// TODO(jsign): switch to union?
+pub const Transaction = struct {
+    type: u8,
+    chain_id: u256,
+    nonce: u64,
+    gas_price: u256,
+    value: u256,
+    to: ?Address,
+    data: []const u8,
+    gas_limit: u64,
+
+    // TODO(jsign): comment about data ownership.
+    pub fn init(type_: u8, chain_id: u256, nonce: u64, gas_price: u256, value: u256, to: ?Address, data: []const u8, gas_limit: u64) Transaction {
+        return Transaction{
+            .type = type_,
+            .chain_id = chain_id,
+            .nonce = nonce,
+            .gas_price = gas_price,
+            .value = value,
+            .to = to,
+            .data = data,
+            .gas_limit = gas_limit,
+        };
+    }
+
+    // TODO(jsign): use some secp256k1 library.
+    pub fn get_from(_: *const Transaction) Address {
+        const from: Address = comptime blk: {
+            var buf: Address = undefined;
+            _ = std.fmt.hexToBytes(&buf, "a94f5374Fce5edBC8E2a8697C15331677e6EbF0B") catch unreachable;
+            break :blk buf;
+        };
+        return from;
+    }
+
+    // TODO(jsign): add helper to get txn hash.
+};
 
 pub const AccountState = struct {
     const log = std.log.scoped(.account_state);
