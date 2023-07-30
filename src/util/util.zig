db9535d13b32050f4fa662b4967c4e616eee751a
Ignacio Hagopian <jsign.uy@gmail.com>
2023-07-30 13:45:11 -0300

    util: fixes

--- src/util/util.zig
+++ src/util/util.zig
@@ -1,9 +1,10 @@
 const std = @import("std");
-const vm = @import("../vm/types.zig");
+const vmtypes = @import("../vm/vm.zig").types;
+const Address = vmtypes.Address;
 
-pub fn hex_to_address(comptime account_hex: []const u8) vm.Address {
+pub fn hex_to_address(account_hex: []const u8) Address {
     const account_hex_strip = if (std.mem.startsWith(u8, account_hex, "0x")) account_hex[2..] else account_hex[0..];
-    var account = std.mem.zeroes([32]u8);
-    _ = std.fmt.hexToBytes(&account, account_hex_strip) catch unreachable;
-    return account;
+    var address = std.mem.zeroes(Address);
+    _ = std.fmt.hexToBytes(&address, account_hex_strip) catch unreachable;
+    return address;
 }
