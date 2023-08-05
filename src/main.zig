6627768464ed9dc67c10861b683cc20a060e13bd
Ignacio Hagopian <jsign.uy@gmail.com>
2023-08-05 16:26:31 -0300

    main: start changing example to use VM abstraction
    Signed-off-by: Ignacio Hagopian <jsign.uy@gmail.com>

--- src/main.zig
+++ src/main.zig
@@ -1,65 +1,68 @@
 const std = @import("std");
-const evmonehost = @import("vm/host.zig");
-const evmc = @cImport({
-    @cInclude("evmc/evmc.h");
-});
-const evmone = @cImport({
-    @cInclude("evmone.h");
-});
 const types = @import("types/types.zig");
 const AccountState = types.AccountState;
-const StateDB = @import("vm/vm.zig").StateDB;
+const Address = types.AccountState;
+const VM = @import("vm/vm.zig").VM;
+const StateDB = @import("vm/statedb.zig");
+const Transaction = @import("types/types.zig").Transaction;
 
 pub fn main() !void {
-    std.log.info("Welcome to phant! 🐘", .{});
-
-    const vm = evmone.evmc_create_evmone();
-    if (vm == null) {
-        @panic("Failed to create EVMOne VM");
-    }
-    std.log.info("evmone info: name={s}, version={s}, abi_version={d}", .{ vm.*.name, vm.*.version, vm.*.abi_version });
-
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     var allocator = gpa.allocator();
+
+    std.log.info("Welcome to phant! 🐘", .{});
+
     var statedb = try StateDB.init(allocator, &[_]AccountState{});
-    var host = evmonehost.init(&statedb);
+    const vm = VM.init(&statedb);
+    _ = vm;
+
+    const txn = Transaction{
+        .type = 0,
+        .chain_id = 1,
+        .nonce = 0,
+        .gas_price = 10,
+        .value = 0,
+        .to = comptime blk: {
+            var addr: Address = undefined;
+            _ = std.fmt.hexToBytes(&addr, "4200000000000000000000000000000000000000000000000000000000000041");
+
+            break :blk addr;
+        },
+        .data = &[_]u8{},
+        .gas_limit = 10_000,
+    };
+    _ = txn;
 
     const code = [_]u8{
-        0x61, 0x41, 0x42,
-        0x31,
+        0x61, 0x41, 0x42, // PUSH2 0x4142
+        // 0x31, // BALANCE
     };
+    _ = code;
 
-    const addr = evmc.struct_evmc_address{
-        .bytes = [_]u8{0x0} ** 10 ++ [_]u8{ 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x10 },
-    };
-    const addr2 = evmc.struct_evmc_address{
-        .bytes = [_]u8{0x0} ** 10 ++ [_]u8{ 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x11 },
-    };
+    //     const message = evmc.struct_evmc_message{
+    //         .kind = evmc.EVMC_CALL,
+    //         .flags = 0,
+    //         .depth = 0,
+    //         .gas = 10_000,
+    //         .recipient = addr2,
+    //         .sender = addr,
+    //         .input_data = &[_]u8{},
+    //         .input_size = 0,
+    //         .value = .{
+    //             .bytes = [_]u8{0} ** 32,
+    //         },
+    //         .create2_salt = .{
+    //             .bytes = [_]u8{0} ** 32,
+    //         },
+    //         .code_address = addr2,
+    //     };
+    //     std.log.info("0x{} bytecode: {} (PUSH2 0x4142; BALANCE;)", .{ std.fmt.fmtSliceHexLower(&message.recipient.bytes), std.fmt.fmtSliceHexLower(&code) });
+    //     std.log.info("executing message -> gas={}, sender=0x{}, recipient=0x{}", .{ message.gas, std.fmt.fmtSliceHexLower(&message.sender.bytes), std.fmt.fmtSliceHexLower(&message.recipient.bytes) });
 
-    const message = evmc.struct_evmc_message{
-        .kind = evmc.EVMC_CALL,
-        .flags = 0,
-        .depth = 0,
-        .gas = 10_000,
-        .recipient = addr2,
-        .sender = addr,
-        .input_data = &[_]u8{},
-        .input_size = 0,
-        .value = .{
-            .bytes = [_]u8{0} ** 32,
-        },
-        .create2_salt = .{
-            .bytes = [_]u8{0} ** 32,
-        },
-        .code_address = addr2,
-    };
-    std.log.info("0x{} bytecode: {} (PUSH2 0x4142; BALANCE;)", .{ std.fmt.fmtSliceHexLower(&message.recipient.bytes), std.fmt.fmtSliceHexLower(&code) });
-    std.log.info("executing message -> gas={}, sender=0x{}, recipient=0x{}", .{ message.gas, std.fmt.fmtSliceHexLower(&message.sender.bytes), std.fmt.fmtSliceHexLower(&message.recipient.bytes) });
-
-    if (vm.*.execute) |exec| {
-        var result = exec(vm, @ptrCast(&host.evmc_host), null, evmc.EVMC_SHANGHAI, @ptrCast(&message), @ptrCast(&code), code.len);
-        std.log.info("execution result -> status_code={}, gas_left={}", .{ result.status_code, result.gas_left });
-    }
+    //    if (vm.*.execute) |exec| {
+    //         var result = exec(vm, @ptrCast(&host.evmc_host), null, evmc.EVMC_SHANGHAI, @ptrCast(&message), @ptrCast(&code), code.len);
+    //         std.log.info("execution result -> status_code={}, gas_left={}", .{ result.status_code, result.gas_left });
+    //     }
 }
 
 test "tests" {
