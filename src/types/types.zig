2e19d985d244778b8a89bd07605f413942f10397
Ignacio Hagopian <jsign.uy@gmail.com>
2023-12-06 09:54:53 -0300

    types: export transactions
    Signed-off-by: Ignacio Hagopian <jsign.uy@gmail.com>

--- src/types/types.zig
+++ src/types/types.zig
@@ -7,10 +7,21 @@ pub const Bytes31 = [31]u8;
 pub const Bytecode = []const u8;
 pub const Address = [20]u8;
 
+// State.
 pub const AccountState = @import("account_state.zig");
-pub const Transaction = @import("transaction.zig");
+
+// Blocks
 pub const Block = @import("block.zig").Block;
 pub const BlockHeader = @import("block.zig").Header;
 pub const Withdrawal = @import("withdrawal.zig");
 
 pub const empty_uncle_hash = @import("block.zig").empty_uncle_hash;
+
+// Transactions
+const transaction = @import("transaction.zig");
+pub const SignatureValues = transaction.SignatureValues;
+pub const Txn = transaction.Txn;
+pub const TxnTypes = transaction.TxnTypes;
+pub const LegacyTxn = transaction.LegacyTxn;
+pub const AccessListTxn = transaction.AccessListTxn;
+pub const MarketFeeTxn = transaction.MarketFeeTxn;
