d76935c1227ee2ea185b885d5ad464509d129995
Ignacio Hagopian <jsign.uy@gmail.com>
2024-01-05 09:21:17 -0300

    util: move package to common
    Signed-off-by: Ignacio Hagopian <jsign.uy@gmail.com>

--- src/common/common.zig
+++ src/common/common.zig
@@ -2,3 +2,4 @@ const hexutils = @import("./hexutils.zig");
 pub const prefixedhex2hash = hexutils.prefixedhex2hash;
 pub const prefixedhex2byteslice = hexutils.prefixedhex2byteslice;
 pub const prefixedhex2u64 = hexutils.prefixedhex2u64;
+pub const hexToAddress = hexutils.hexToAddress;
--- src/util/util.zig
+++
@@ -1,9 +0,0 @@
-const std = @import("std");
-const Address = @import("../types/types.zig").Address;
-
-pub fn hex_to_address(account_hex: []const u8) Address {
-    const account_hex_strip = if (std.mem.startsWith(u8, account_hex, "0x")) account_hex[2..] else account_hex[0..];
-    var address = std.mem.zeroes(Address);
-    _ = std.fmt.hexToBytes(&address, account_hex_strip) catch unreachable;
-    return address;
-}
