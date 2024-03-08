e67735456da485e4fe79037c46753faf2914db81
Ignacio Hagopian <jsign.uy@gmail.com>
2024-03-08 12:37:13 -0300

    mpt: support empty tree
    Signed-off-by: Ignacio Hagopian <jsign.uy@gmail.com>

--- src/mpt/mpt.zig
+++ src/mpt/mpt.zig
@@ -1,8 +1,35 @@
 const std = @import("std");
 const rlp = @import("zig-rlp");
 const types = @import("../types/types.zig");
+const common = @import("../common/common.zig");
 const Allocator = std.mem.Allocator;
 const Hash32 = types.Hash32;
+
+pub const empty_mpt_root = common.comptimeHexToBytes("56e81f171bcc55a6ff8345e692c0f86e5b48e01b996cadc001622fb5e363b421");
+
+const KeyVal = struct {
+    key: []const u8,
+    value: []const u8,
+};
+
+fn mptize(list: []KeyVal) Hash32 {
+    const s = struct {
+        fn lessThan(_: void, a: KeyVal, b: KeyVal) bool {
+            return std.mem.lessThan(u8, a.key, b.key);
+        }
+    };
+    std.sort.pdq(KeyVal, list, {}, s.lessThan);
+
+    return getMPTRoot(list, 0);
+}
+
+fn getMPTRoot(list: []KeyVal, level: usize) Hash32 {
+    _ = level;
+    if (list.len == 0) {
+        return empty_mpt_root;
+    }
+    return [_]u8{0} ** 32;
+}
 
 // indexToRLP returns the RLP representation of the index.
 // The caller is responsible for freeing the returned slice.
@@ -32,13 +59,19 @@ test "basic" {
     var arena = std.heap.ArenaAllocator.init(std.testing.allocator);
     defer arena.deinit();
     const allocator = arena.allocator();
+    _ = allocator;
 
-    for (0..1000) |i| {
-        for (0..i) |index| {
-            const key = try indexToRLP(allocator, @intCast(index));
-            const value = key;
-            _ = value; // Just set the value as the key.
+    const TestCase = struct {
+        name: []const u8,
+        keyvals: []KeyVal,
+        exp_hash: Hash32,
+    };
+    const test_cases = [_]TestCase{
+        .{ .name = "empty", .keyvals = &[_]KeyVal{}, .exp_hash = empty_mpt_root },
+    };
 
-        }
+    inline for (test_cases) |tc| {
+        const got = mptize(tc.keyvals);
+        try std.testing.expectEqualSlices(u8, &tc.exp_hash, &got);
     }
 }
