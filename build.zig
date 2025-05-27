dd4c55ec7334bc7beb8ab1bf60a9ac531f314e72
Guillaume Ballet <3272758+gballet@users.noreply.github.com>
2025-05-27 22:33:26 +0200

    Apply suggestions from code review

--- build.zig
+++ build.zig
@@ -94,7 +94,6 @@ pub fn build(b: *std.Build) !void {
     exe.addIncludePath(b.path("evmone/evmc/include"));
     exe.addLibraryPath(b.path("zig-out/evmone_build/lib"));
     exe.linkSystemLibrary("evmone");
-    // exe.linkCxxAbi();
     exe.linkLibC();
     exe.root_module.addImport("zig-rlp", dep_rlp.module("zig-rlp"));
     exe.linkLibrary(depSecp256k1.artifact("secp256k1"));
@@ -141,7 +140,6 @@ pub fn build(b: *std.Build) !void {
     });
     unit_tests.addLibraryPath(b.path("zig-out/evmone_build/lib"));
     unit_tests.linkSystemLibrary("evmone");
-    // exe.linkCxxAbi();
     unit_tests.linkLibC();
     unit_tests.addIncludePath(b.path("evmone/include/evmone"));
     unit_tests.addIncludePath(b.path("evmone/evmc/include"));
